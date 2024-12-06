f4cdb5b62f03ee3eda30e5262f1c78eaf8e17d7b
Timor990 <105429089+Timor990@users.noreply.github.com>
2024-12-06 17:33:44 +0800

    feat: 车辆动画实现

--- Scripts/VehicleController.cs
+++ Scripts/VehicleController.cs
@@ -7,11 +7,36 @@ public class VehicleController : MonoBehaviour
     public Vector3 speedVector=Vector3.zero;
     private float wheelRadius = 0.3f;
     private const float PI = 3.14f;
+    private List<Transform> wheelList =new List<Transform>();
     [SerializeField]
     private float wheelSpeed = 0;
+    private Animator anim;
+    public float speed;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     void Update() {
-        wheelSpeed = Vector3.Magnitude(speedVector)/(2* PI* wheelRadius) *360;
-        
 
     }
+    private void LateUpdate()
+    {
+        wheelSpeed = Vector3.Magnitude(speedVector) / (2 * PI * wheelRadius) * 360;
+        handleWheelRotate();
+        Debug.Log(transform.forward);
+        Debug.Log(transform.right);
+
+        Debug.Log(Vector3.Angle(transform.forward, speedVector));
+        Debug.Log(Mathf.Sign(Mathf.Sin(Vector3.Angle(transform.forward, speedVector))));
+        anim.SetFloat("Speed", Mathf.Sign(Mathf.Sin(Vector3.Angle(transform.right, speedVector))) * Vector3.Magnitude(speedVector));
+    }
+    void Move() { 
+    }
+    void handleWheelRotate() {
+        for (int i = 0; i < 4; i++) {
+            
+        }
+
+    }
+
 }
